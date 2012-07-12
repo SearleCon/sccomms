7271f8c64733ef33d495d0b94c4277f6279fa584
Searle Consulting <shaun@searleconsulting.co.za>
2012-07-12 12:46:19 +0200

    Finally got jQuery and Bootstrap working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,10 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require twitter/bootstrap
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
 //= require_tree .
-$('#example').tooltip(options)
+$(document).ready(function() {
+  $('#myCarousel').carousel({ interval: 2000 })
+});
